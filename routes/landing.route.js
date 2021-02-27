2ade080d8e4891c4278d190e418e48688eedd03e
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-02-27 04:09:36 -0500

    hope this works... extra /api route

--- routes/landing.route.js
+++ routes/landing.route.js
@@ -2,8 +2,6 @@ const express           = require("express");
 const router = express.Router();
 var userController  = require('../controller/user-controller');
 
-router.get('/', (req, res) => { res.send('This means that this is working!')});
-
 router.post('/', userController.loginUser);
 // router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 //   return res.json({ msg: `Hey ${req.user.email}! I open at the close.` });
