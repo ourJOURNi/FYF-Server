540d397071dd535e0f1cbec1399461bab90c0fc1
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-03-16 22:57:09 -0400

    added get route

--- routes/landing.route.js
+++ routes/landing.route.js
@@ -2,6 +2,11 @@ const express           = require("express");
 const router = express.Router();
 var userController  = require('../controller/user-controller');
 
+router.get('/', (req, res) => {
+  console.log('Getting from API!')
+  res.status(200).json({msg: 'I did it!'});
+}
+)
 router.post('/', userController.loginUser);
 // router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 //   return res.json({ msg: `Hey ${req.user.email}! I open at the close.` });
